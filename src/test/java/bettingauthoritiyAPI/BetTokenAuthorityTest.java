d4cad61a5c96081e298b39467962a9a8438b2755
Abdirimov,Maruf M <m.abdirimov@student.fontys.nl>
2020-03-21 22:53:31 +0100

    Create one test to check how CI works

--- src/test/java/bettingauthoritiyAPI/BetTokenAuthorityTest.java
+++ src/test/java/bettingauthoritiyAPI/BetTokenAuthorityTest.java
@@ -1,7 +1,13 @@
 package bettingauthoritiyAPI;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BetTokenAuthorityTest {
-
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
 }
