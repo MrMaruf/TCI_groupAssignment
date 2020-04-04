6d9c28253d23bb87798fcc5b9697782741fc28b0
GalinKarafiziev <galin.karafiziew@gmail.com>
2020-04-04 22:06:01 +0200

    First cycle of TDD for the test Game.startBettingRound (redone, because i didnt read the java dos correctly)

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,15 +1,9 @@
 import ID.BettingRoundID;
 import bettingauthoritiyAPI.BetToken;
-import bettingauthoritiyAPI.BetTokenAuthority;
 import bettingauthoritiyAPI.BettingAuthority;
 import bettingauthoritiyAPI.IBetTokenAuthority;
 import casino.game.IBettingRound;
-import casino.game.IGame;
 import casino.game.IGameRule;
-import org.junit.Assert;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 import static org.mockito.Mockito.*;
 
@@ -19,16 +13,21 @@ public void Game_startBettingRound_Should_Create_bettingRound_And_Log_Info_Test(
     BettingAuthority betAuth = mock(BettingAuthority.class);
     IGameRule gRule = mock(IGameRule.class);
     Game sut = new Game(gRule, betAuth);
-    IBetTokenAuthority betToken = mock(IBetTokenAuthority.class);
+    IBetTokenAuthority betTokenAuth = mock(IBetTokenAuthority.class);
     IBettingRound betR = mock(IBettingRound.class);
+    BettingRoundID betRID = mock(BettingRoundID.class);
+    BetToken betToken = mock(BetToken.class);
 
     //act
 
-    when(betAuth.getTokenAuthority()).thenReturn((betToken);
-    when(sut.betR = betR).thenReturn(betR);
+    when(betR.getBettingRoundID()).thenReturn(betRID);
+    when(betAuth.getTokenAuthority().getBetToken(betRID)).thenReturn(betToken);
+    when(sut.isBettingRoundFinished()).thenReturn(true);
+    when(sut.getBettingRound()).thenReturn(betR);
     sut.startBettingRound();
 
     //assert
+    verify(sut.getBettingRound()).equals(betR);
     verify(betAuth.getLoggingAuthority()).startBettingRound(betR);
 
 
