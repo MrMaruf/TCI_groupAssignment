e25fb99abe15ba25d2b094e4c211024f04c5f759
GalinKarafiziev <galin.karafiziew@gmail.com>
2020-04-05 09:09:30 +0200

    First cycle of TDD for the test of Player.removePlayerCard

--- src/test/java/playertests/PlayerTest.java
+++ src/test/java/playertests/PlayerTest.java
@@ -16,14 +16,32 @@ import static org.mockito.Mockito.*;
 public class PlayerTest {
     @Test
     public void Player_addPlayerCard_Should_Add_PlayerCard_To_The_List_of_playerCards_Test(){
+        //arrange
         ICasino casino = mock(ICasino.class);
         IPlayer sut = new Player(casino);
 
         IPlayerCard card = new PlayerCard();
 
+        //act
         sut.addPlayerCard(card);
 
+        //assert
         Assert.assertEquals(1, sut.getAllPlayerCards().size());
 
+    }
+
+    @Test
+    public void Player_removePlayerCard_Should_Remove_PlayerCard_From_The_List_of_PlayerCards_Test(){
+       //arrange
+        ICasino casino = mock(ICasino.class);
+        IPlayer sut = new Player(casino);
+        IPlayerCard card = mock(PlayerCard.class);
+
+        //act
+        sut.addPlayerCard(card);
+        sut.removePlayerCard(card);
+
+        //assert
+        Assert.assertEquals(0, sut.getAllPlayerCards().size() );
     }
 }
