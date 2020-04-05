6cde9246b59db4b40dd8335d9b673868f2a59408
Georgi Kostov <kostov.georgi11@gmail.com>
2020-04-05 11:35:22 +0200

    Cashier distriburePlayerCard Success: Adding Functionality so that test works

---
+++ src/main/java/casino/cashier/Cashier.java
@@ -0,0 +1,49 @@
+package casino.cashier;
+
+import bettingauthoritiyAPI.BettingAuthority;
+import casino.bet.Bet;
+import casino.bet.MoneyAmount;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Cashier implements ICashier {
+    HashMap<IPlayerCard, MoneyAmount> moneyPerPlayerCard;
+    BettingAuthority betAuth;
+
+    public Cashier(List<IPlayerCard> cards, BettingAuthority betAuth){
+        this.moneyPerPlayerCard = new HashMap<>();
+        for(IPlayerCard pc: cards){
+            this.moneyPerPlayerCard.put(pc, null);
+        }
+        this.betAuth = betAuth;
+    }
+
+    @Override
+    public IPlayerCard distributeGamblerCard() {
+        for(IPlayerCard pc: this.moneyPerPlayerCard.keySet()){
+            if(this.moneyPerPlayerCard.get(pc) == null) {
+                this.moneyPerPlayerCard.put(pc, new MoneyAmount(0));
+                this.betAuth.getLoggingAuthority().handOutGamblingCard(pc.getCardID());
+                return pc;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void returnGamblerCard(IPlayerCard card) {
+
+    }
+
+    @Override
+    public boolean checkIfBetIsValid(IPlayerCard card, Bet betToCheck) {
+        return false;
+    }
+
+    @Override
+    public void addAmount(IPlayerCard card, MoneyAmount amount) {
+
+    }
+}
--- src/test/java/casino/cashier/CashierTest.java
+++ src/test/java/casino/cashier/CashierTest.java
@@ -2,27 +2,36 @@ package casino.cashier;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.mockito.Mockito.*;
-
 import bettingauthoritiyAPI.BettingAuthority;
+import bettingauthoritiyAPI.IBetLoggingAuthority;
+import casino.idbuilder.ids.CardID;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CashierTest {
     @Test
     public void distributePlayerCard_Should_Return_IPlayerCard_Object_And_Log_Info_Test(){
         // arrange
-        boolean hasGottenACard = false;
+        IBetLoggingAuthority bl = mock(IBetLoggingAuthority.class);
         BettingAuthority betAuth = mock(BettingAuthority.class);
-        ICashier c = new Cashier(betAuth);
+        when(betAuth.getLoggingAuthority()).thenReturn(bl);
+
+        List<IPlayerCard> cards = new ArrayList<>();
+        cards.add(mock(IPlayerCard.class));
+        ICashier c = new Cashier(cards, betAuth);
         IPlayerCard emptyCard;
+
 
         // act
         emptyCard = c.distributeGamblerCard();
 
         // assert
         Assert.assertThat(emptyCard, instanceOf(IPlayerCard.class));
-        verify(betAuth).getLoggingAuthority().handOutGamblingCard(emptyCard.getCardID());
+        verify(bl).handOutGamblingCard(emptyCard.getCardID());
     }
 
 }
