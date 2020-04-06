0b5073a19be26848d0c778c0ae6ce2e9b6bd8c59
GalinKarafiziev <galin.karafiziew@gmail.com>
2020-04-06 16:17:34 +0200

    Negative tests

--- src/test/java/gametests/GameTest.java
+++ src/test/java/gametests/GameTest.java
@@ -1,5 +1,6 @@
 package gametests;
 
+import ID.BettingRoundID;
 import bettingauthoritiyAPI.*;
 import casino.bet.Bet;
 import casino.bet.BetResult;
@@ -9,8 +10,10 @@ import casino.game.Game;
 import casino.game.IGameRule;
 import casino.gamingmachine.GamingMachine;
 import casino.gamingmachine.IGamingMachine;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.rules.ExpectedException;
 
 import java.util.*;
 
@@ -18,15 +21,18 @@ import static org.mockito.Mockito.*;
 
 
 public class GameTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void Game_startBettingRound_Should_Create_Betting_Round_And_Log_Info_Test(){
+    public void Game_startBettingRound_Should_Create_Betting_Round_And_Log_Info_Test() {
         IGameRule gameRule = mock(IGameRule.class);
         BettingAuthority bettingAuthority = new BettingAuthority();
         BettingRound betRound = mock(BettingRound.class);
         Game sut = new Game(gameRule, bettingAuthority);
 
         sut.setBettingRound(betRound);
-        if(sut.getBettingRound() != null){
+        if (sut.getBettingRound() != null) {
             sut.bettingAuthority.getLoggingAuthority().startBettingRound(betRound);
         }
 
@@ -35,8 +41,8 @@ public class GameTest {
     }
 
     @Test
-    public void Game_determineWinner_should_find_betResult_and_send_it_to_gamingmachine_test(){
-       //arrange
+    public void Game_determineWinner_should_find_betResult_and_send_it_to_gamingmachine_test() {
+        //arrange
         GamingMachine gm = mock(GamingMachine.class);
         IGameRule gameRule = mock(IGameRule.class);
         BettingAuthority bettingAuthority = new BettingAuthority();
@@ -54,11 +60,10 @@ public class GameTest {
 
         BetResult finalBet = new BetResult(bet, new MoneyAmount(i));
         sut.setBettingRound(betRound);
-        if(sut.isBettingRoundFinished() == false){
+        if (sut.isBettingRoundFinished() == false) {
             bets.add(bet);
             bets.add(bet1);
             sut.addGamingMachine(gm);
-
 
 
             sut.bettingAuthority.getTokenAuthority().getRandomInteger(token);
@@ -66,11 +71,10 @@ public class GameTest {
             when(gameRule.getMaxBetsPerRound()).thenReturn(1);
             when(gameRule.determineWinner(gameRule.getMaxBetsPerRound(), betRound.getAllBetsMade())).thenReturn(finalBet);
 
-            for(IGamingMachine gameMachine: sut.getGamingMachines()){
+            for (IGamingMachine gameMachine : sut.getGamingMachines()) {
                 gameMachine.acceptWinner(gameRule.determineWinner(gameRule.getMaxBetsPerRound(), betRound.getAllBetsMade()));
 
             }
-
 
 
             sut.determineWinner();
@@ -91,7 +95,7 @@ public class GameTest {
     }
 
     @Test
-    public void Game_isBettingFinished_Should_Return_ValueOf_property_same_name_Test(){
+    public void Game_isBettingFinished_Should_Return_ValueOf_property_same_name_Test() {
         IGameRule gameRule = mock(IGameRule.class);
         BettingAuthority bettingAuthority = new BettingAuthority();
         Game sut = new Game(gameRule, bettingAuthority);
@@ -100,8 +104,9 @@ public class GameTest {
 
         Assert.assertEquals(sut.isBettingRoundFinished(), true);
     }
+
     @Test
-    public void Game_acceptBet_Should_Pass_Bet_To_BettingRound_Log_Info_Test(){
+    public void Game_acceptBet_Should_Pass_Bet_To_BettingRound_Log_Info_Test() {
         //arrange
         GamingMachine gm = mock(GamingMachine.class);
         IGameRule gameRule = mock(IGameRule.class);
@@ -114,20 +119,85 @@ public class GameTest {
         //act
         sut.setBettingRound(betRound);
         sut.setIsBettingFinished(true);
-        if(sut.isBettingRoundFinished()){
-            when(betRound.placeBet(bet)).thenReturn(true);
+        if (sut.isBettingRoundFinished()) {
+            when(betRound.placeBet(any(Bet.class))).thenReturn(true);
             sut.bettingAuthority.getLoggingAuthority().addAcceptedBet(bet, betRound.getBettingRoundID(), gm.getGamingMachineID());
             sut.acceptBet(bet, gm);
 
         }
 
 
-
-
-
-
-
         Assert.assertEquals(true, sut.acceptBet(bet, gm));
 
     }
-}
+
+    @Test
+    public void Game_acceptBet_Should_Raise_NoCurrentBettingRound_ShouldThrow_NullPointer_Exception_test() throws Exception {
+        exception.expect(NullPointerException.class);
+
+
+        GamingMachine gm = mock(GamingMachine.class);
+        IGameRule gameRule = mock(IGameRule.class);
+        BettingAuthority bettingAuthority = new BettingAuthority();
+        BetToken token = mock(BetToken.class);
+        Game sut = new Game(gameRule, bettingAuthority);
+        BettingRound betRound = null;
+        Bet bet = mock(Bet.class);
+
+        //act
+        sut.setBettingRound(betRound);
+        sut.setIsBettingFinished(true);
+        if (sut.isBettingRoundFinished()) {
+            when(betRound.placeBet(any(Bet.class))).thenReturn(true);
+            sut.bettingAuthority.getLoggingAuthority().addAcceptedBet(bet, betRound.getBettingRoundID(), gm.getGamingMachineID());
+            sut.acceptBet(bet, gm);
+
+        }
+    }
+
+    @Test
+    public void Game_DetermineWinner_While_Round_Isnt_Started_Should_Return_Null_test() throws Exception {
+        exception.expect(NullPointerException.class);
+        GamingMachine gm = mock(GamingMachine.class);
+        IGameRule gameRule = mock(IGameRule.class);
+        BettingAuthority bettingAuthority = new BettingAuthority();
+        BetToken token = mock(BetToken.class);
+        Game sut = new Game(gameRule, bettingAuthority);
+        BettingRound betRound = mock(BettingRound.class);
+        Bet bet = mock(Bet.class);
+
+        sut.setBettingRound(betRound);
+        sut.setIsBettingFinished(true);
+        if (sut.isBettingRoundFinished()) {
+            throw new NullPointerException("no betting round has started yet");
+        }
+    }
+
+
+        @Test
+    public void Game_IsbettingFinished_While_BettingRound_Is_set__Should_return_false_test() throws Exception{
+            exception.expect(NullPointerException.class);
+            GamingMachine gm = mock(GamingMachine.class);
+            IGameRule gameRule = mock(IGameRule.class);
+            BettingAuthority bettingAuthority = new BettingAuthority();
+            BetToken token = mock(BetToken.class);
+            Game sut = new Game(gameRule, bettingAuthority);
+            BettingRound betRound = mock(BettingRound.class);
+            Bet bet = mock(Bet.class);
+
+            sut.setBettingRound(new BettingRound(new BetToken(new BettingRoundID())));
+            if(sut.getBettingRound() == null) {
+                sut.setBettingRound(betRound);
+                sut.setIsBettingFinished(false);
+            }else{
+                throw new NullPointerException("Sorry, you cannot start a new round before the current ends");
+            }
+
+
+            }
+
+
+        }
+
+
+
