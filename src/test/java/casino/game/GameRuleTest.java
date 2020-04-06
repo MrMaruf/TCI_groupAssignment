e5c82e3611c0e0c2f851f4d22c590ed98e1adf63
Gogo <kostov.georgi11@gmail.com>
2020-04-06 16:22:21 +0200

    GameRule detirmineWinner Negative: Added non passign test

--- src/test/java/casino/game/GameRuleTest.java
+++ src/test/java/casino/game/GameRuleTest.java
@@ -15,7 +15,7 @@ public class GameRuleTest {
     @Test
     public void determineWinner_Should_Return_The_Correct_betResult_Test(){
         // assign
-        IGameRule rule = new GameRule(2);
+        IGameRule rule = new GameRule(5);
         LinkedHashSet<Bet> bets = new LinkedHashSet<>();
         // mocking
         bets.add(mock(Bet.class));
@@ -25,11 +25,24 @@ public class GameRuleTest {
         bets.add(mock(Bet.class));
         bets.add(mock(Bet.class));
 
-        //act
+        // act
         BetResult result = rule.determineWinner(1, bets);
 
         // assert
         Assert.assertEquals(result.getWinningBet(), winningBet);
         Assert.assertEquals(result.getAmountWon().getAmountInCents(), 200);
+    }
+
+    @Test
+    public void determineWinner_With_No_Bets_Should_Return_Null_Test(){
+        // assign
+        IGameRule rule = new GameRule(5);
+        LinkedHashSet<Bet> bets = new LinkedHashSet<>();
+
+        // act
+        BetResult result = rule.determineWinner(1, bets);
+
+        // assert
+        Assert.assertNull(result);
     }
 }
